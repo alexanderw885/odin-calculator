4349ce0135f5fc33c52bb78875db4f7c59d09f45
Alexander Williams <alexanderw885@gmail.com>
2026-07-23 14:58:27 -0700

    add button and simplify layout

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const CALC_WIDTH = 350
+const CALC_HEIGHT = 400;
 
 // Dynamically set up calculator width
 const calc = document.querySelector("#calc");
 calc.style.width = CALC_WIDTH + 'px';
+calc.style.height = CALC_HEIGHT + 'px';
 
-const smallButtons = document.querySelectorAll("#left .small");
+
+const button_div = document.querySelector("#buttons");
+button_div.style.height = (CALC_HEIGHT * 5 / 6) + 'px';
+
+const smallButtons = document.querySelectorAll(".small");
 smallButtons.forEach((button) => 
-    button.style.flexBasis = (CALC_WIDTH / 5) + 'px');
+    button.style.flexBasis = (CALC_HEIGHT / 7) + 'px');
 
-const largeButtons = document.querySelectorAll("#left .large");
+const largeButtons = document.querySelectorAll(".large");
 largeButtons.forEach((button) =>
-    button.style.flexBasis = (CALC_WIDTH * 2 / 5) + 'px');
+    button.style.flexBasis = (CALC_HEIGHT / 3) + 'px');
 
 
 
