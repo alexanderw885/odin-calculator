45e8a0905ce8075d702c2ce84606a16b4caf2790
Alexander Williams <alexanderw885@gmail.com>
2026-07-23 13:13:10 -0700

    display calculator on screen

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const CALC_WIDTH = 350
+
+// Dynamically set up calculator width
+const calc = document.querySelector("#calc");
+calc.style.width = CALC_WIDTH + 'px';
+
+const smallButtons = document.querySelectorAll("#left .small");
+smallButtons.forEach((button) => 
+    button.style.flexBasis = (CALC_WIDTH / 5) + 'px');
+
+const largeButtons = document.querySelectorAll("#left .large");
+largeButtons.forEach((button) =>
+    button.style.flexBasis = (CALC_WIDTH * 2 / 5) + 'px');
+
+
+
 let val1;
 let val2;
 let op;
